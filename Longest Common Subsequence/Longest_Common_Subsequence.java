b5e0fda7a4fbee19ab1a3c8d16735776d272369e
Lance M. Peterson <lancepeterson50@gmail.com>
2020-04-26 15:49:17 -0500

    Update Longest_Common_Subsequence.java

--- Longest Common Subsequence/Longest_Common_Subsequence.java
+++ Longest Common Subsequence/Longest_Common_Subsequence.java
@@ -1,15 +1,12 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
-        //Dynamic Programming Solution
-        char [] ar1 = text1.toCharArray();
-        char [] ar2 = text2.toCharArray();
-        int dp [][] = new int [ar1.length][ar2.length];
-        return LCS(ar1, ar2, 0, 0, dp);
-        //Iterative Solution
-        //return iterativeLCS(text1, text2);
+        // char [] ar1 = text1.toCharArray();
+        // char [] ar2 = text2.toCharArray();
+        // int dp [][] = new int [ar1.length][ar2.length];
+        // return LCS(ar1, ar2, 0, 0, dp);
+        return iterativeLCS(text1, text2);
     }
-    
-    //Dynamic Programming with Memoization (19 ms)
+	//Dynamic Programming with Memoization (~19 ms)
     public int LCS(char [] ar1, char [] ar2, int i, int j, int [][] dp){
         if (i == ar1.length || j == ar2.length) {
 			return 0;
@@ -29,20 +26,19 @@ class Solution {
         dp[i][j] = result;
 		return result;
     }
-    
-    //Bottom-Up Iterative (6 ms)
+	//Bottom-Up Iterative (~6 ms)
     public int iterativeLCS(String text1, String text2) {
         char [] ar1 = text1.toCharArray();
         char [] ar2 = text2.toCharArray();
         int dp [][] = new int [ar1.length + 1][ar2.length + 1];
         for (int i = 1; i < dp.length; i++) {
         	for (int j = 1; j < dp[i].length; j++) {
-				if (ar1[i-1] == ar2[j-1]){
-					dp[i][j] = 1 + dp[i-1][j-1];  
+                if (ar1[i-1] == ar2[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1];  
                 } else {
-					dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
                 }
-			}
+        	}
         }
         return dp[ar1.length][ar2.length];
     }
