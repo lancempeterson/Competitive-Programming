25db57270388e4eb8ddcba1fc927de8f3363399a
lancempeterson <lancepeterson50@gmail.com>
2020-08-02 16:24:17 -0500

    Update Move_Zeroes.java

--- Coding Problems & My Solutions/Move Zeroes/Move_Zeroes.java
+++ Coding Problems & My Solutions/Move Zeroes/Move_Zeroes.java
@@ -1,26 +1,17 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        if ( nums.length == 0 || nums.length == 1 ){
-            return;
-        }
         int a = 0;
-        int b = 1;
-        while ( b < nums.length ){
-            if ( nums[b] != 0 && nums[a] == 0 ){
-                int temp = nums[b];
-                nums[b] = nums[a];
-                nums[a] = temp;
+        int b = 0;
+        while (b < nums.length){
+            if (nums[a] == 0 && nums[b] != 0){
+                int temp = nums[a];
+                nums[a] = nums[b];
+                nums[b] = temp;
+            }
+            if (nums[a] != 0 || nums[b] != 0){
                 a++;
-                b++;
             }
-            else if ( nums[b] == 0 && nums[a] == 0 ){
-                b++;
-            }
-            else {
-                a++;
-                b++;
-            }
+            b++;
         }
-        return;
     }
 }
