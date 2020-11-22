b0d06064e659459a4ed3f8a80faff1451dae6406
lancempeterson <lancepeterson50@gmail.com>
2020-11-21 19:47:44 -0600

    Update Two_Sum.java

--- Coding Problems & My Solutions/Two Sum/Two_Sum.java
+++ Coding Problems & My Solutions/Two Sum/Two_Sum.java
@@ -2,13 +2,13 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
         int [] ans = new int [2];
         HashMap<Integer, Integer> map = new HashMap<>();
-        for ( int i = 0; i < nums.length; i++ ){
+        for (int i = 0; i < nums.length; i++){
             int x = nums[i];
             int y = target - x;
-            if ( map.containsKey(y) ){
+            if (map.containsKey(y)){
                 ans[0] = i;
                 ans[1] = map.get(y);
-                return ans;
+                break;
             }
             map.put(x, i);
         }
