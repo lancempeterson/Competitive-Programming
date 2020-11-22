9d4fb88b28926ac951e51030e5452aa4b00788a3
lancempeterson <lancepeterson50@gmail.com>
2020-11-22 13:41:09 -0600

    Update Best_Time_To_Buy_And_Sell_Stock.java

--- Coding Problems & My Solutions/Best Time to Buy and Sell Stock/Best_Time_To_Buy_And_Sell_Stock.java
+++ Coding Problems & My Solutions/Best Time to Buy and Sell Stock/Best_Time_To_Buy_And_Sell_Stock.java
@@ -1,13 +1,15 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int highestValue = 0;
-        for ( int i = 0; i < prices.length; i++ ){
-            for ( int j = i; j < prices.length; j++ ){
-                if ( prices[j] - prices[i] > highestValue ){
-                     highestValue = prices[j] - prices[i];
-                 }
+        int minPrice = Integer.MAX_VALUE;
+        int max = 0;
+        for (int i = 0; i < prices.length; i++){
+            if (i > 0){
+                int spentCost = 0 - minPrice;
+                int priceGain = spentCost + prices[i];
+                max = Math.max(max, priceGain);
             }
+            minPrice = Math.min(minPrice, prices[i]);
         }
-        return highestValue;
+        return max;
     }
 }
