03a689161cf8938253fe1404e1a8e8abcc1161cb
lancempeterson <lancepeterson50@gmail.com>
2020-11-23 12:04:56 -0600

    Update Contains_Duplicate.java

--- Coding Problems & My Solutions/Contains Duplicate/Contains_Duplicate.java
+++ Coding Problems & My Solutions/Contains Duplicate/Contains_Duplicate.java
@@ -1,10 +1,10 @@
 public boolean containsDuplicate(int[] nums) {
         HashSet<Integer> set = new HashSet<>();
-        for (int i : nums){
-            if (set.contains(i)){
+        for (int n : nums){
+            if (set.contains(n)){
                 return true;
             } else {
-                set.add(i);
+                set.add(n);
             }
         }
         return false;
