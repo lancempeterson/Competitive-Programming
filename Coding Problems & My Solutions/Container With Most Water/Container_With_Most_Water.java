7e5fb44f44a54d61b013e729cccb119a3c37ca43
lancempeterson <lancepeterson50@gmail.com>
2020-11-27 12:35:05 -0600

    Update Container_With_Most_Water.java

--- Coding Problems & My Solutions/Container With Most Water/Container_With_Most_Water.java
+++ Coding Problems & My Solutions/Container With Most Water/Container_With_Most_Water.java
@@ -1,14 +1,14 @@
 class Solution {
     public int maxArea(int[] height) {
         int max = 0;
-        int l = 0;
-        int r = height.length - 1;
-        while (l < r){
-            max = Math.max(max, (r - l) * Math.min(height[l], height[r]));
-            if (height[l] <= height[r]){
-                l++;
+        int a = 0;
+        int b = height.length - 1;
+        while (a < b){
+            max = Math.max(max,(Math.min(height[a],height[b])*(b-a)));
+            if (height[a] > height[b]){
+                b--;
             } else{
-                r--;
+                a++;
             }
         }
         return max;
