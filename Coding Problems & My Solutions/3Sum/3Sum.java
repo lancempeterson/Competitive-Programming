166c77804b8b4d70956120ca1ac55b201f7a3d8b
lancempeterson <lancepeterson50@gmail.com>
2020-11-29 16:14:10 -0600

    Update 3Sum.java

--- Coding Problems & My Solutions/3Sum/3Sum.java
+++ Coding Problems & My Solutions/3Sum/3Sum.java
@@ -1,40 +1,40 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        if ( nums.length < 3 ){
+        if (nums == null || nums.length < 3){
             return ans;
         }
         Arrays.sort(nums);
-		for (int i = 0; i < nums.length-2; i++) {
-			if ( i == 0 || nums[i] > nums[i-1] ) {
-                int target = 0 - nums[i];
-			    int left = i+1;
-			    int right = nums.length-1;
-                while (left < right){
-                    int val = nums[left] + nums[right];
-			
-			        if ( val == target ) {
-				        List<Integer> list = new ArrayList<>();
-				        list.add(nums[i]);
-				        list.add(nums[left]);
-				        list.add(nums[right]);
-				        ans.add(list);
-			        }
-			        if ( val > target ) {
-                        int currentRight = right;
-                        while ( nums[right] == nums[currentRight] && left < right){
-                            right--;
-                        }
-			        }
-			        else {
-                        int currentLeft = left;
-                        while (nums[left] == nums[currentLeft] && left < right){
-                            left++;    
-                        }
-			        }
+        for (int i = 0; i < nums.length-2; i++){
+            if (i != 0 && nums[i] == nums[i-1]){
+                continue;
+            }
+            int z = nums[i];
+            int a = i+1;
+            int b = nums.length-1;
+            while (a < b){
+                int x = nums[a];
+                int y = nums[b];
+                if (x + y + z == 0){
+                    List<Integer> l = new ArrayList<>();
+                    l.add(z);
+                    l.add(x);
+                    l.add(y);
+                    ans.add(l);
+                }
+                if (x + y + z > 0){
+                    int currentY = y;
+                    while(currentY == nums[b] && b > a){
+                        b--;
+                    }
+                } else {
+                    int currentX = x;
+                    while (currentX == nums[a] && a < b){
+                        a++;
+                    }
                 }
             }
-		}
+        }
         return ans;
     }
 }
