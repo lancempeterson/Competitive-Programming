d2047f3e5e18a4fc1bd838588f814232e05ce96c
lancempeterson <lancepeterson50@gmail.com>
2020-11-30 14:38:52 -0600

    Update Valid_Anagram.java

--- Coding Problems & My Solutions/Valid Anagram/Valid_Anagram.java
+++ Coding Problems & My Solutions/Valid Anagram/Valid_Anagram.java
@@ -1,15 +1,15 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()){
+        if (s == null || t == null || s.length() != t.length()){
             return false;
         }
-        int[] alphFreq = new int [26];
-        for(char c : s.toCharArray()){
-            alphFreq[c - 'a']++;
+        int[] alphabetFreq = new int[26];
+        for (char c : s.toCharArray()){
+            alphabetFreq[c-'a']++;
         }
         for (char c : t.toCharArray()){
-            alphFreq[c - 'a']--;
-            if (alphFreq[c - 'a'] < 0){
+            alphabetFreq[c-'a']--;
+            if (alphabetFreq[c-'a'] < 0){
                 return false;
             }
         }
