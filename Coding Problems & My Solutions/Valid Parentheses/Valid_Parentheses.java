6f8ff1e4817259016ff528e24c965eda95bfdb9d
lancempeterson <lancepeterson50@gmail.com>
2020-12-01 23:34:08 -0600

    Update Valid_Parentheses.java

--- Coding Problems & My Solutions/Valid Parentheses/Valid_Parentheses.java
+++ Coding Problems & My Solutions/Valid Parentheses/Valid_Parentheses.java
@@ -1,48 +1,30 @@
 class Solution {
     public boolean isValid(String s) {
-        if ( s.equals("")) {
-			return true;
-		}
         Stack<Character> stack = new Stack<>();
-        for ( int i = 0; i < s.length(); i++) {
-			char c = s.charAt(i);
-			if ( c == '(' || c == '{' || c == '[' ) {
-				stack.push(c);
-			}
-			else if ( c == ')') {
-				if ( !stack.isEmpty() ) {
-					char d = stack.pop();
-					if ( d != '(' ) {
-						return false;
-					}
-				}
-                else {
-                    return false;
-                }
-			}
-			else if ( c == '}') {
-				if ( !stack.isEmpty() ) {
-					char d = stack.pop();
-					if ( d != '{' ) {
-						return false;
-					}
-				}
-                else {
-                    return false;
-                }
-			}
-			else if ( c == ']') {
-				if ( !stack.isEmpty() ) {
-					char d = stack.pop();
-					if ( d != '[' ) {
-						return false;
-					}
-				}
-                else {
-                    return false;
-                }
-			}
-		}
+        for (char c : s.toCharArray()){
+            switch(c){
+                case '(':
+                case '{':
+                case '[':
+                    stack.push(c);
+                    break;
+                case ')':
+                    if (stack.isEmpty() || stack.pop() != '('){
+                        return false;
+                    }
+                    break;
+                case '}':
+                    if (stack.isEmpty() || stack.pop() != '{'){
+                        return false;
+                    }
+                    break;
+                case ']':
+                    if (stack.isEmpty() || stack.pop() != '['){
+                        return false;
+                    }
+                    break;
+            }
+        }
         if (!stack.isEmpty()){
             return false;
         }
