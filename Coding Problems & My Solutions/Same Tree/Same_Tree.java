f98fdb5f0282c1b6fd82fad5d6dbfc1d16fd74dc
lancempeterson <lancepeterson50@gmail.com>
2021-02-06 20:54:25 -0600

    Same Tree

--- Coding Problems & My Solutions/Same Tree/Same_Tree.java
+++ Coding Problems & My Solutions/Same Tree/Same_Tree.java
@@ -4,6 +4,35 @@
  *     int val;
  *     TreeNode left;
  *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null){
+            return true;
+        }
+        else if (p == null || q == null || (p.val != q.val)){
+            return false;
+        }
+        return (p.val == q.val) 
+            && isSameTree(p.left, q.left) 
+            && isSameTree(p.right, q.right);
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
  *     TreeNode(int x) { val = x; }
  * }
  */
@@ -27,5 +56,40 @@ class Solution {
             return false;
         }
         return ans;
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null){
+            return true;
+        }
+        else if (p == null || q == null || (p.val != q.val)){
+            return false;
+        }
+        boolean leftSame = (p.left == null) && (q.left == null);
+        boolean rightSame = (p.right == null) && (q.right == null);
+        if (p.left != null && q.left != null){
+            leftSame = isSameTree(p.left, q.left);
+        }
+        if (p.right != null && q.right != null){
+            rightSame = isSameTree(p.right, q.right);
+        }
+        return (p.val == q.val) && leftSame && rightSame;
     }
 }
