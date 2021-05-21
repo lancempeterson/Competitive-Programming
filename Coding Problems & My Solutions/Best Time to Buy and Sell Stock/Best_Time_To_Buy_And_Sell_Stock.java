a64484ac3a805f8b6af959c5268e03e1de8777a6
lancempeterson <lancepeterson50@gmail.com>
2021-05-21 15:04:36 -0500

    Update Best_Time_To_Buy_And_Sell_Stock.java

--- Coding Problems & My Solutions/Best Time to Buy and Sell Stock/Best_Time_To_Buy_And_Sell_Stock.java
+++ Coding Problems & My Solutions/Best Time to Buy and Sell Stock/Best_Time_To_Buy_And_Sell_Stock.java
@@ -13,3 +13,18 @@ class Solution {
         return max;
     }
 }
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length < 1) {
+            return 0;
+        }
+        int max = 0;
+        int low = prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            low = Math.min(low, prices[i - 1]);
+            max = Math.max(max, prices[i] - low);
+        }
+        return max;
+    }
+}
