edff702df2c5345ce0b303f4d72dd0ee7b71abbc
lancempeterson <lancepeterson50@gmail.com>
2021-05-22 19:19:37 -0500

    Update Two_Sum.java

--- Coding Problems & My Solutions/Two Sum/Two_Sum.java
+++ Coding Problems & My Solutions/Two Sum/Two_Sum.java
@@ -15,3 +15,21 @@ class Solution {
         return ans;
     }
 }
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int[] ans = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                ans[0] = i;
+                ans[1] = map.get(nums[i]);
+                break;
+            } else {
+                int pairNum = target - nums[i];
+                map.put(pairNum, i);
+            }
+        }
+        return ans;
+    }
+}
