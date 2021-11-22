01f787ffeae3eaae1f7533dd0d906ccbf0f3aa3f
lancempeterson <lancepeterson50@gmail.com>
2021-11-22 14:25:25 -0600

    Update Valid_Anagram.kt

--- Coding Problems & My Solutions/Valid Anagram/Valid_Anagram.kt
+++ Coding Problems & My Solutions/Valid Anagram/Valid_Anagram.kt
@@ -3,6 +3,25 @@ class Solution {
         if (s.length != t.length) {
             return false
         }
+        var alphFreq = IntArray(26)
+        for (c in s.toCharArray()) {
+            alphFreq[c - 'a'] += 1
+        }
+        for (c in t.toCharArray()) {
+            alphFreq[c - 'a'] -= 1
+            if (alphFreq[c - 'a'] < 0) {
+                return false
+            }
+        }
+        return true
+    }
+}
+
+class Solution {
+    fun isAnagram(s: String, t: String): Boolean {
+        if (s.length != t.length) {
+            return false
+        }
         val sSorted: CharArray = s.toCharArray().sortedArray()
         val tSorted: CharArray = t.toCharArray().sortedArray()
         var i = 0
