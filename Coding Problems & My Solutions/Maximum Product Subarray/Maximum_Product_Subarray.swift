5b0bcacb1cd68d9f7551ab16875bb661bfd3b8ac
lancempeterson <lancepeterson50@gmail.com>
2023-01-09 13:57:30 -0600

    Update Maximum_Product_Subarray.swift

--- Coding Problems & My Solutions/Maximum Product Subarray/Maximum_Product_Subarray.swift
+++ Coding Problems & My Solutions/Maximum Product Subarray/Maximum_Product_Subarray.swift
@@ -12,3 +12,18 @@ class Solution {
         return ans
     }
 }
+
+class Solution {
+    func maxProduct(_ nums: [Int]) -> Int {
+        var ans = Int.min
+        var currentMax = 1
+        var currentMin = 1
+        for num in nums {
+            var tempMax = currentMax
+            currentMax = max(num, num * currentMax, num * currentMin)
+            currentMin = min(num, num * tempMax, num * currentMin)
+            ans = max(ans, num, currentMax)
+        }
+        return ans
+    }
+}
