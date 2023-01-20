74d11cbcaa75c646e76d98cba226541884555198
lancempeterson <lancepeterson50@gmail.com>
2023-01-20 17:53:20 -0600

    Update Longest_Substring_Without_Repeating_Characters.swift

--- Coding Problems & My Solutions/Longest Substring Without Repeating Characters/Longest_Substring_Without_Repeating_Characters.swift
+++ Coding Problems & My Solutions/Longest Substring Without Repeating Characters/Longest_Substring_Without_Repeating_Characters.swift
@@ -18,3 +18,25 @@ class Solution {
         return maxLen
     }
 }
+
+class Solution {
+    func lengthOfLongestSubstring(_ s: String) -> Int {
+        var set = Set<Character>()
+        var ans = 0
+        let charArray = Array(s)//s.map { String($0) }
+        var a = 0
+        var b = 0
+        while b < charArray.count {
+            if set.contains(charArray[b]) {
+                set.remove(charArray[a])
+                a += 1
+            } else {
+                set.insert(charArray[b])
+                let currentLen = (b - a) + 1
+                ans = max(ans, currentLen)
+                b += 1
+            }
+        }
+        return ans
+    }
+}
