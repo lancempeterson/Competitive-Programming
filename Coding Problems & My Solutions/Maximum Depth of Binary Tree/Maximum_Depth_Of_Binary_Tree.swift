1a0f4e15efeed7117afafe4f3f7f27ada84f1cfd
lancempeterson <lancepeterson50@gmail.com>
2023-03-05 17:47:04 -0600

    Update Maximum_Depth_Of_Binary_Tree.swift

--- Coding Problems & My Solutions/Maximum Depth of Binary Tree/Maximum_Depth_Of_Binary_Tree.swift
+++ Coding Problems & My Solutions/Maximum Depth of Binary Tree/Maximum_Depth_Of_Binary_Tree.swift
@@ -23,3 +23,32 @@ class Solution {
         return max(leftMax, rightMax)
     }
 }
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func maxDepth(_ root: TreeNode?) -> Int {
+        guard let root = root else {
+            return 0
+        }
+        if root.left == nil && root.right == nil {
+            return 1
+        }
+        let l = maxDepth(root.left)
+        let r = maxDepth(root.right)
+        return 1 + max(l, r)
+    }
+}
