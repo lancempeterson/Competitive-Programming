7292d37aac37db72790bcdf45a21a470ef8f57ad
lancempeterson <lancepeterson50@gmail.com>
2023-03-04 20:17:58 -0600

    Swift

--- Coding Problems & My Solutions/Contiguous Array/Continguous_Array.swift
+++ Coding Problems & My Solutions/Contiguous Array/Continguous_Array.swift
@@ -42,3 +42,25 @@ class Solution {
         return maxx
     }
 }
+
+class Solution {
+    func findMaxLength(_ nums: [Int]) -> Int {
+        var count: Int = 0
+        var dict: [Int:Int] = [:]
+        var ans: Int = 0
+        dict[0] = -1
+        for (i, num) in nums.enumerated() {
+            if num == 0 {
+                count -= 1
+            } else {
+                count += 1
+            }
+            if let firstIndexOfCount = dict[count] {
+                ans = max(ans, (i - firstIndexOfCount))
+            } else {
+                dict[count] = i
+            }
+        }
+        return ans
+    }
+}
--- Coding Problems & My Solutions/Maximum Subarray/Maximum_Subarray.swift
+++ Coding Problems & My Solutions/Maximum Subarray/Maximum_Subarray.swift
@@ -10,3 +10,17 @@ class Solution {
         return maximum
     }
 }
+
+class Solution {
+    func maxSubArray(_ nums: [Int]) -> Int {
+        if nums.isEmpty { return 0 }
+        var count: Int = 0
+        var ans: Int = nums.first!
+        for num in nums {
+            let prevBestChoice = max(0, count)
+            count = prevBestChoice + num
+            ans = max(ans, count)
+        }
+        return ans
+    }
+}
