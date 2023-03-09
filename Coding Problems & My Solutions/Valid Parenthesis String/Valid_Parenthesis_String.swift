fe43ff0543a020fe2b058b949370808272bca4c3
lancempeterson <lancepeterson50@gmail.com>
2023-03-09 14:29:45 -0600

    Update Valid_Parenthesis_String.swift

--- Coding Problems & My Solutions/Valid Parenthesis String/Valid_Parenthesis_String.swift
+++ Coding Problems & My Solutions/Valid Parenthesis String/Valid_Parenthesis_String.swift
@@ -32,3 +32,38 @@ class Solution {
         return lrStack.isEmpty
     }
 }
+
+class Solution {
+    func checkValidString(_ s: String) -> Bool {
+        var leftParaStack: [Int] = []
+        var starStack: [Int] = []
+        for (i, char) in Array(s).enumerated() {
+            if char == ")" {
+                if leftParaStack.isEmpty && starStack.isEmpty {
+                    return false
+                }
+                if leftParaStack.isEmpty == false {
+                    leftParaStack.popLast()
+                } 
+                else {
+                    starStack.popLast()
+                }
+            }
+            else if char == "(" {
+                leftParaStack.append(i)
+            }
+            else {
+                starStack.append(i)
+            }
+        }
+        while !leftParaStack.isEmpty && !starStack.isEmpty {
+            if leftParaStack.last! < starStack.last! {
+                leftParaStack.popLast()
+                starStack.popLast()
+            } else {
+                break
+            }
+        }
+        return leftParaStack.isEmpty
+    }
+}
