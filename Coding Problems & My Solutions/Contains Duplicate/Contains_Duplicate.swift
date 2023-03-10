56c63ce9bf2793eb46c4c5129696d2ef39d3f134
lancempeterson <lancepeterson50@gmail.com>
2023-03-09 21:23:20 -0600

    Update Contains_Duplicate.swift

--- Coding Problems & My Solutions/Contains Duplicate/Contains_Duplicate.swift
+++ Coding Problems & My Solutions/Contains Duplicate/Contains_Duplicate.swift
@@ -8,3 +8,16 @@ class Solution {
         return false
     }
 }
+
+class Solution {
+    func containsDuplicate(_ nums: [Int]) -> Bool {
+        var set = Set<Int>()
+        for num in nums {
+            if set.contains(num) {
+                return true
+            }
+            set.insert(num)
+        }
+        return false
+    }
+}
