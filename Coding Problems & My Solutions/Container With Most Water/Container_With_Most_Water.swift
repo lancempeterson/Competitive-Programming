5d79f4fbc502f57c2a6017aca239017daca514a3
lancempeterson <lancepeterson50@gmail.com>
2023-03-30 11:52:13 -0500

    Update Container_With_Most_Water.swift

--- Coding Problems & My Solutions/Container With Most Water/Container_With_Most_Water.swift
+++ Coding Problems & My Solutions/Container With Most Water/Container_With_Most_Water.swift
@@ -14,3 +14,21 @@ class Solution {
         return maxNo
     }
 }
+
+class Solution {
+    func maxArea(_ height: [Int]) -> Int {
+        var a = 0
+        var b = height.count - 1
+        var ans = 0
+        while a < b {
+            let localArea = min(height[a], height[b]) * (b - a)
+            ans = max(ans, localArea)
+            if height[a] > height[b] {
+                b -= 1
+            } else {
+                a += 1
+            }
+        }
+        return ans
+    }
+}
